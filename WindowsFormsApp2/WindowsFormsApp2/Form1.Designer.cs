d444328a359e9cb678853947e7757559e379a4b2
Jasmine79 <55802301+Jasmine79@users.noreply.github.com>
2019-11-13 19:33:05 +0100

    Add files via upload

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_Nev = new System.Windows.Forms.TextBox();
+            this.button_hozzaad = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.radioButton_N = new System.Windows.Forms.RadioButton();
+            this.radioButton_F = new System.Windows.Forms.RadioButton();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button_Mentes = new System.Windows.Forms.Button();
+            this.button_Betolt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Név:";
+            // 
+            // textBox_Nev
+            // 
+            this.textBox_Nev.Location = new System.Drawing.Point(90, 24);
+            this.textBox_Nev.Name = "textBox_Nev";
+            this.textBox_Nev.Size = new System.Drawing.Size(202, 32);
+            this.textBox_Nev.TabIndex = 1;
+            // 
+            // button_hozzaad
+            // 
+            this.button_hozzaad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button_hozzaad.Location = new System.Drawing.Point(312, 27);
+            this.button_hozzaad.Name = "button_hozzaad";
+            this.button_hozzaad.Size = new System.Drawing.Size(84, 29);
+            this.button_hozzaad.TabIndex = 2;
+            this.button_hozzaad.Text = "Hozzáad";
+            this.button_hozzaad.UseVisualStyleBackColor = true;
+            this.button_hozzaad.Click += new System.EventHandler(this.button_hozzaad_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(90, 117);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(202, 229);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // radioButton_N
+            // 
+            this.radioButton_N.AutoSize = true;
+            this.radioButton_N.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.radioButton_N.Location = new System.Drawing.Point(90, 74);
+            this.radioButton_N.Name = "radioButton_N";
+            this.radioButton_N.Size = new System.Drawing.Size(47, 24);
+            this.radioButton_N.TabIndex = 4;
+            this.radioButton_N.TabStop = true;
+            this.radioButton_N.Tag = "Nő";
+            this.radioButton_N.Text = "Nő";
+            this.radioButton_N.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_F
+            // 
+            this.radioButton_F.AutoSize = true;
+            this.radioButton_F.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.radioButton_F.Location = new System.Drawing.Point(182, 74);
+            this.radioButton_F.Name = "radioButton_F";
+            this.radioButton_F.Size = new System.Drawing.Size(59, 24);
+            this.radioButton_F.TabIndex = 5;
+            this.radioButton_F.TabStop = true;
+            this.radioButton_F.Tag = "Férfi";
+            this.radioButton_F.Text = "Férfi";
+            this.radioButton_F.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button_Mentes
+            // 
+            this.button_Mentes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button_Mentes.Location = new System.Drawing.Point(314, 78);
+            this.button_Mentes.Name = "button_Mentes";
+            this.button_Mentes.Size = new System.Drawing.Size(81, 28);
+            this.button_Mentes.TabIndex = 6;
+            this.button_Mentes.Text = "Mentés";
+            this.button_Mentes.UseVisualStyleBackColor = true;
+            this.button_Mentes.Click += new System.EventHandler(this.button_Mentes_Click);
+            // 
+            // button_Betolt
+            // 
+            this.button_Betolt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button_Betolt.Location = new System.Drawing.Point(315, 127);
+            this.button_Betolt.Name = "button_Betolt";
+            this.button_Betolt.Size = new System.Drawing.Size(80, 29);
+            this.button_Betolt.TabIndex = 7;
+            this.button_Betolt.Text = "Betöltés";
+            this.button_Betolt.UseVisualStyleBackColor = true;
+            this.button_Betolt.Click += new System.EventHandler(this.button_Betolt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1444, 865);
+            this.Controls.Add(this.button_Betolt);
+            this.Controls.Add(this.button_Mentes);
+            this.Controls.Add(this.radioButton_F);
+            this.Controls.Add(this.radioButton_N);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button_hozzaad);
+            this.Controls.Add(this.textBox_Nev);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Fájlkezelés";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_Nev;
+        private System.Windows.Forms.Button button_hozzaad;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.RadioButton radioButton_N;
+        private System.Windows.Forms.RadioButton radioButton_F;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button_Mentes;
+        private System.Windows.Forms.Button button_Betolt;
+    }
+}
+
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_hozzaad_Click(object sender, EventArgs e)
+        {
+            string Sor = textBox_Nev.Text.Trim();
+            //-- a textBox szöveg tartalma az elejéről és végéről leszedi a szóközt
+            if (radioButton_N.Checked) 
+                //-- a változó ki van -e jelölve egy logikai értéket ad vissza
+            {
+                Sor += " - " + radioButton_N.Tag;
+            }
+            else
+            {
+                Sor += " - " + radioButton_F.Tag;
+            }
+
+            listBox1.Items.Add(Sor);
+            textBox_Nev.Text = ""; //--kiürítjük a textBox beviteli mezőt
+        }
+
+        private void button_Mentes_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "Minden fájl (*.*)|*.*|Szövegfájl (*.txt)|*.txt";
+            //-- a szűrésekhez használjuk: 1-1 szűrés 2 részből áll
+            //-- 1. felh.nak szól, 2. op.rsz.-nek szól a kettőt az AltGr+W (|) választja el
+            saveFileDialog1.FilterIndex = 2;
+            //-- az =2 azt jelenti, hogy a második lehetőség lesz az alapért.
+            
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK) //--megtörtént-e a mentés
+            {
+                try
+                {
+                    using (StreamWriter sw = new StreamWriter(saveFileDialog1.FileName))
+                    //--a saveFileDialog1.FileName- mel kiolvassuk a file helyét és nevét
+                    {
+                        foreach (string item in listBox1.Items)
+                        {
+                            sw.WriteLine(item);
+                        }
+                    }
+                }
+                catch (IOException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    throw;
+                }
+            }
+            //-- saveFileDialog1.ShowDialog(); 
+            //--Mentés másként megnyitása, 
+            //--filekiterjesztést nem tudunk választani
+        }
+
+        private void button_Betolt_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Minden fájl (*.*)|*.*|Szövegfájl (*.txt)|*.txt";
+            //-- a Filter-t a szűrésekhez használjuk: 1-1 szűrés 2 részből áll
+            //-- 1. felh.nak szól, 2. op.rsz.-nek szól a két részt és aszűrőket is az AltGr+W (|) választja el
+            openFileDialog1.FilterIndex = 2;
+            //-- az =2 azt jelenti, hogy a második lehetőség lesz az alapért.
+            openFileDialog1.InitialDirectory = Environment.SpecialFolder.MyDocuments.ToString();
+            //-- alapértelmezett alkvt. megadása
+            openFileDialog1.RestoreDirectory = false;
+            //--a megelőző könyvtár meghatározását megjegyezze-e
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+            try
+            {
+                using (StreamReader sr=new StreamReader(openFileDialog1.FileName))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        listBox1.Items.Add(sr.ReadLine()); 
+                        //-- a listBox elemeihez adjuk hozzá soronként
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox list = (ListBox)sender;
+            //--teljes biztonsági másolatot hozunk létre, 
+            //--hogy az eseménykezelésekhez is hozzáférjünk
+            string[] sor = list.SelectedItem.ToString().Split('-');
+            //-- annak a karaktersorozatnak az értéke melyre rákattintott a felh.
+            textBox_Nev.Text = sor[0].Trim();
+            if (radioButton_F.Tag.Equals(sor[1].Trim()))
+            {
+                radioButton_F.Checked = true;
+                radioButton_N.Checked = false;
+            }
+            if (radioButton_N.Tag.Equals(sor[1].Trim()))
+            {
+                radioButton_F.Checked = false;
+                radioButton_N.Checked = true;
+            }
+            
+
+        }
+    }
+}
